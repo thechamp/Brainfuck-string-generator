c5d133b43bdc5b380f08fc6e7bc97b1e380ac9de
Abhay <abhay_1303@yahoo.co.in>
2012-11-15 01:54:35 +0530

    header module with function declarations

---
+++ src/converter_helper.h
@@ -0,0 +1,13 @@
+/*
+ * Author       :  Abhay Jain
+ * File         :  converter_helper.h
+ * Description  :  This header module contains the declaration
+                   for all functions used in project.
+ * Created on   :  14 Nov 2012
+ * Language     :  C
+ */
+ 
+int generate_bf_equivalent(int, int, int, int, char);
+void print_division_by_2(int);
+void print_multiplication_by_2(int);
+void print_remainder_steps(int, char);
